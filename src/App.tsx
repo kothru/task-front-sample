39766e1b9eb4668c5163ec342ee5ce833a03c176
kothru <1894683+kothru@users.noreply.github.com>
2022-04-02 23:05:49 +0900

    add table

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import {
   gql
 } from "@apollo/client";
 
+type TaskType = {
+  id: number
+  name: string
+  plan: number
+  actual: number
+}
+
 const GET_TASKS = gql`
   query GetTasks {
     tasks {
@@ -14,14 +21,40 @@ const GET_TASKS = gql`
   }
 `;
 
-function Tasks() {
-  const { loading, error, data } = useQuery(GET_TASKS);
+function TaskTable() {
+  const { loading, error, data } = useQuery<TaskType[]>(GET_TASKS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
+  if (data === undefined) return <p>No Data</p>;
+
+  const listItems = () => {
+    return data.map((task) =>
+      <tr key={task.id}>
+        <td>{task.id}</td>
+        <td>{task.name}</td>
+        <td>{task.plan}</td>
+        <td>{task.actual}</td>
+      </tr>
+    );
+  }
 
   return (
-    <div></div>
+    <div className="overflow-x-auto">
+      <table className="table table-compact w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Plan</th>
+            <th>Actual</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listItems}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
@@ -29,7 +62,7 @@ function App() {
   return (
     <div>
       <h2>My first Apollo app 🚀</h2>
-      <Tasks />
+      <TaskTable />
     </div>
   )
 }
